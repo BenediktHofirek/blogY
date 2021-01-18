b59899fb8b38bb3fc9b9fa64548755aecbefe526
utility4u <benedikthofirek@gmail.com>
2021-01-18 16:48:43 +0100

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { AuthModule } from './features/auth/auth.module';
-import { HomeModule } from './features/home/home.module';
-import { UserModule } from './features/user/user.module';
-import { MaterialModule } from './shared/modules/material-module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+  ],
   imports: [
-    AuthModule,
-    CoreModule,
-    HomeModule,
     SharedModule,
-    UserModule,
-    MaterialModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
